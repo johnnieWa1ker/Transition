d26a9a63fcc686f83e6da8ca00fac089d22ee09d
Johnnie Walker <rockwave.fm@gmail.com>
2023-01-28 20:15:06 +0700

    [TR-1] Fixes

--- Sources/Transition/Modal/BottomSheetProps.swift
+++ Sources/Transition/Modal/BottomSheetProps.swift
@@ -11,41 +11,41 @@ import UIKit
 public struct BottomSheetProps {
     /// A Boolean value indicating whether the view controller enforces a modal behavior.
     ///
-    /// The default value of this property is false. When you set it to true, UIKit ignores events outside the view controller's bounds and prevents the interactive dismissal of the view controller while it is onscreen.
-    let isModalInPresentation: Bool
+    /// The default value of this property is `false`. When you set it to `true`, UIKit ignores events outside the view controller's bounds and prevents the interactive dismissal of the view controller while it is onscreen.
+    public let isModalInPresentation: Bool
     
     /// The array of heights where a sheet can rest.
     ///
     /// The default value is an array that contains the value large(). This array must contain at least one element. When you set this value, specify detents in order from smallest to largest height.
-    let detents: [UISheetPresentationController.Detent]
+    public let detents: [UISheetPresentationController.Detent]
     
     /// The identifier of the most recently selected detent.
     ///
-    /// This property represents the most recent detent that the user selects or that you set programmatically. The default value is nil, which means the sheet displays at the smallest detent you specify in detents.
-    let selectedDetentIdentifier: UISheetPresentationController.Detent.Identifier?
+    /// This property represents the most recent detent that the user selects or that you set programmatically. The default value is `nil`, which means the sheet displays at the smallest detent you specify in detents.
+    public let selectedDetentIdentifier: UISheetPresentationController.Detent.Identifier?
     
     /// A Boolean value that determines whether scrolling expands the sheet to a larger detent.
     ///
-    /// The default value is true, which means if the sheet can expand to a larger detent than selectedDetentIdentifier, scrolling up in the sheet increases its detent instead of scrolling the sheet’s content. After the sheet reaches its largest detent, scrolling begins.
-    /// Set this value to false if you want to avoid letting a scroll gesture expand the sheet. For example, you can set this value on a nonmodal sheet to avoid obscuring the content underneath the sheet.
-    let prefersScrollingExpandsWhenScrolledToEdge: Bool
+    /// The default value is `true`, which means if the sheet can expand to a larger detent than selectedDetentIdentifier, scrolling up in the sheet increases its detent instead of scrolling the sheet’s content. After the sheet reaches its largest detent, scrolling begins.
+    /// Set this value to `false` if you want to avoid letting a scroll gesture expand the sheet. For example, you can set this value on a nonmodal sheet to avoid obscuring the content underneath the sheet.
+    public let prefersScrollingExpandsWhenScrolledToEdge: Bool
     
     /// The largest detent that doesn’t dim the view underneath the sheet.
     ///
-    /// The default value is nil, which means the system adds a noninteractive dimming view underneath the sheet at all detents. Set this property to only add the dimming view at detents larger than the detent you specify. For example, set this property to medium to add the dimming view at the large detent.
+    /// The default value is `nil`, which means the system adds a noninteractive dimming view underneath the sheet at all detents. Set this property to only add the dimming view at detents larger than the detent you specify. For example, set this property to medium to add the dimming view at the large detent.
     /// Without a dimming view, the undimmed area around the sheet responds to user interaction, allowing for a nonmodal experience. You can use this behavior for sheets with interactive content underneath them.
-    let largestUndimmedDetentIdentifier: UISheetPresentationController.Detent.Identifier?
+    public let largestUndimmedDetentIdentifier: UISheetPresentationController.Detent.Identifier?
     
     /// The corner radius that the sheet attempts to present with.
     ///
-    /// The default value is nil. This property only has an effect when the sheet is at the front of its sheet stack.
-    let preferredCornerRadius: CGFloat?
+    /// The default value is `nil`. This property only has an effect when the sheet is at the front of its sheet stack.
+    public let preferredCornerRadius: CGFloat?
     
     /// A Boolean value that determines whether the sheet shows a grabber at the top.
     ///
-    /// The default value is false, which means the sheet doesn’t show a grabber. A grabber is a visual affordance that indicates that a sheet is resizable. Showing a grabber may be useful when it isn’t apparent that a sheet can resize or when the sheet can’t dismiss interactively.
-    /// Set this value to true for the system to draw a grabber in the standard system-defined location. The system automatically hides the grabber at appropriate times, like when the sheet is full screen in a compact-height size class or when another sheet presents on top of it.
-    let prefersGrabberVisible: Bool
+    /// The default value is `false`, which means the sheet doesn’t show a grabber. A grabber is a visual affordance that indicates that a sheet is resizable. Showing a grabber may be useful when it isn’t apparent that a sheet can resize or when the sheet can’t dismiss interactively.
+    /// Set this value to `true` for the system to draw a grabber in the standard system-defined location. The system automatically hides the grabber at appropriate times, like when the sheet is full screen in a compact-height size class or when another sheet presents on top of it.
+    public let prefersGrabberVisible: Bool
     
     /// - Parameters:
     ///   - isModalInPresentation: A Boolean value indicating whether the view controller enforces a modal behavior.
@@ -55,14 +55,14 @@ public struct BottomSheetProps {
     ///   - largestUndimmedDetentIdentifier: The largest detent that doesn’t dim the view underneath the sheet.
     ///   - preferredCornerRadius: The corner radius that the sheet attempts to present with.
     ///   - prefersGrabberVisible: A Boolean value that determines whether the sheet shows a grabber at the top.
-    init(
+    public init(
         isModalInPresentation: Bool = false,
         detents: [UISheetPresentationController.Detent] = [.large()],
         selectedDetentIdentifier: UISheetPresentationController.Detent.Identifier? = nil,
         prefersScrollingExpandsWhenScrolledToEdge: Bool = true,
-        largestUndimmedDetentIdentifier: UISheetPresentationController.Detent.Identifier?,
+        largestUndimmedDetentIdentifier: UISheetPresentationController.Detent.Identifier? = nil,
         preferredCornerRadius: CGFloat? = nil,
-        prefersGrabberVisible: Bool
+        prefersGrabberVisible: Bool = false
     ) {
         self.isModalInPresentation = isModalInPresentation
         self.detents = detents
